1d36e0be376c304704f5c7593c499b5c5aa06b54
Timea Farkas <145453473+tmea-farkas@users.noreply.github.com>
2024-09-29 13:30:36 +0000

    forms module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
+import ControlledForm from "./components/ControlledForm";
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
+      <ControlledForm />
     </div>
   );
 }
