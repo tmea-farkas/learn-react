ddc6969769875bb30677c6ec084dc686d305410d
Timea Farkas <145453473+tmea-farkas@users.noreply.github.com>
2024-10-13 14:10:32 +0000

    react module finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import css from "./App.module.css";
-import HTTPHooks from "./components/HTTPHooks";
-
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+import ContentAPI from "./components/ContentAPI";
 
 function App() {
   return (
     <div className={css.App}>
-      <HTTPHooks />
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
